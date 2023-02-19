c20f044a544a3206ba284ff0fdf9fa8f19a4fd30
konojack <konojack@gmail.com>
2023-02-19 18:15:42 +0100

    configured axios and all requests to the TMDB - displaying random movie inside the banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,26 @@
+import axios from './axios';
+import requests from './Requests';
+import { useEffect, useState } from 'react';
 import './Banner.css';
 
 const Banner = () => {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+
+      return request;
+    }
+
+    fetchData();
+  }, []);
+
   const truncate = (string, numberOfCharsFromCut) => {
     return string?.length > numberOfCharsFromCut
       ? string.substring(0, numberOfCharsFromCut - 1) + '...'
@@ -13,27 +33,20 @@ const Banner = () => {
       style={{
         backgroundSize: 'cover',
         // backgroundImage: 'url(./netflix-banner.png)',
-        backgroundImage:
-          'url("https://upload.wikimedia.org/wikipedia/commons/c/cd/Black_flag.svg")',
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"`,
         backgroundPosition: 'center center',
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.name || movie?.title || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(
-            `This is a test description This is a test description This is a test
-          description This is a test description This is a test description This
-          is a test description This is a test description This is a test
-          description This is a test description This is a test description This
-          is a test description This is a test description This is a test
-          description This is a test description`,
-            150
-          )}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
 
